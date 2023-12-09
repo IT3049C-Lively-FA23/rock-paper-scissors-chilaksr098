94a86cc9b33adb6fda74053bb4b3a6d2b52f4a0a
sasidhar chilaka <97551417+chilaksr098@users.noreply.github.com>
2023-12-09 11:45:04 -0500

    Update 0.folderStructure.test.js

--- tests/0.folderStructure.test.js
+++ tests/0.folderStructure.test.js
@@ -6,8 +6,7 @@ describe(`folder structure`, function () {
     const rootTree = dirTree(`${__dirname}/..`);
     const nodes = rootTree.children.map(node => node.name);
 
-    expect(nodes).toContain(`resources`);
-    expect(nodes).toContain(`index.html`);
+    
     const expectedNodes = [".git", ".github", ".vscode", "resources", "tests", ".gitignore", ".babelrc", ".eslintrc.json", ".gitignore", ".prettierrc", "jest.config.js", "index.html", "package.json", "README.md"];
     expect(nodes).toEqual(expect.arrayContaining(expectedNodes));
   });
