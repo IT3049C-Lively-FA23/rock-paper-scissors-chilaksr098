02ac8496554d8898b37779d1bf48b3338d5185fb
sasidhar chilaka <97551417+chilaksr098@users.noreply.github.com>
2023-12-09 11:22:22 -0500

    Update rock_paper_scissors.js

--- resources/scripts/rock_paper_scissors.js
+++ resources/scripts/rock_paper_scissors.js
@@ -1,9 +1,10 @@
 class RockPaperScissors {
+
   constructor(username) {
     this.username = username;
     this.score = {
       user: 0,
-      cpu:0 
+      cpu: 0 
     },
     this.gameHistoryLog = [];
   }
@@ -14,9 +15,8 @@ class RockPaperScissors {
    */
   generateCPUResponse(){
     const acceptedValues = [ `rock`, `paper`, `scissors` ];
-
-    return ;
-  }
+    return acceptedValues[Math.floor(Math.random()*acceptedValues.length)];
+  };
   /**
    * returns one of the following values: `win`, `lose`, `tie`
    * tie:
@@ -33,15 +33,50 @@ class RockPaperScissors {
    * @param {string} cpuSelection computer selection. Can only be one of the following values [`rock`, `paper`, `scissors`]
    */
   determineWinner(userSelection, cpuSelection){
+    if (userSelection === cpuSelection) {
+      return 'tie';
+    } else if (userSelection === 'rock') {
+      if (cpuSelection === 'paper') {
+        return 'lose';
+      } else {
+        return 'win';
+      }
+    } else if (userSelection === 'paper'){
+      if (cpuSelection === 'scissors') {
+        return 'lose';
+      }else {
+        return 'win';
+      }
+    } else if (userSelection === 'scissors') {
+      if (cpuSelection === 'rock') {
+        return 'lose';
+      } else {
+        return 'win';
+      }
+    }
+  };
 
-  }
-
-  /**
+  /**[const | let | var] = function () {} (or () =>
    * 
    * @param {string} userSelection user selection. Can only be one of the following values [`rock`, `paper`, `scissors`]
-   */
+  */
   play(userSelection){
+    this.userSelection = userSelection.toLocaleLowerCase();
+    console.log(this.userSelection);
+   
+    this.cpuSelection = this.generateCPUResponse();
+    console.log(this.cpuSelection);
 
+    const result = this.determineWinner(this.userSelection, this.cpuSelection);
+    if(result === 'tie'){
+      this.gameHistoryLog.push(`${this.username} selected ${this.userSelection}, CPU selected ${this.cpuSelection}: tie`);
+    } else if(result === 'lose'){
+      this.score.cpu ++;
+      this.gameHistoryLog.push(`${this.username} selected ${this.userSelection}, CPU selected ${this.cpuSelection}: CUP wins wins`);
+    } else if(result === "win"){
+      this.score.user ++;
+      this.gameHistoryLog.push(`${this.username} selected ${this.userSelection}, CPU selected ${this.cpuSelection}: ${this.username} wins wins`);
+    }
   }
 
 }
